591cdacc5d25fdb44d760c9af6f9d00ccc0b3e57
Keryan SANIE <keryan.sanie@gmail.com>
2015-04-15 15:40:32 +0200

    lol

--- includes/menu_left.php
+++ includes/menu_left.php
@@ -1,3 +1,5 @@
+<!-- j'utilise une librairie qui s'appelle boostrap et qui permet de definir des css de base -->
+<!-- donc quand je met la classe nav il va comprendre que c'est un menu -->
 <div id="navbar" class="span3 nav nav-pills nav-stacked">
 	<ul class="navLeft">
 		<li class="linkLeft"><a href="tableau_de_bord.php">Tableau de bord</a></li>
@@ -6,5 +8,7 @@
 		<li class="linkLeft"><a href="armees.php">Armées</a></li>
 		<li class="linkLeft"><a href="defences.php">Défences</a></li>
 	</ul>
-	<div class="navClose"><a href="#">_</a></div>
+	<!-- ça ça sera pour fermé le menu, ça peiut etre cool (faudra faire en jquery) -->
+	<div class="navClose"><a href="#" class="glyphicon glyphicon-align-justify"></a></div>
 </div>
+
