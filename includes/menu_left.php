fe876186fc22352b83837fdeff021971dbdc2de0
Keryan SANIE <keryan.sanie@gmail.com>
2015-04-15 16:23:14 +0200

    commit avec git GUI

--- includes/menu_left.php
+++ includes/menu_left.php
@@ -1,6 +1,6 @@
 <!-- j'utilise une librairie qui s'appelle boostrap et qui permet de definir des css de base -->
 <!-- donc quand je met la classe nav il va comprendre que c'est un menu -->
-<div id="navbar" class="span3 nav nav-pills nav-stacked">
+<div id="navbar" class="col-md-3 nav nav-pills nav-stacked">
 	<ul class="navLeft">
 		<li class="linkLeft"><a href="tableau_de_bord.php">Tableau de bord</a></li>
 		<li class="linkLeft"><a href="ressources.php">Ressources</a></li>
@@ -8,7 +8,8 @@
 		<li class="linkLeft"><a href="armees.php">Armées</a></li>
 		<li class="linkLeft"><a href="defences.php">Défences</a></li>
 	</ul>
+
+	
 	<!-- ça ça sera pour fermé le menu, ça peiut etre cool (faudra faire en jquery) -->
 	<div class="navClose"><a href="#" class="glyphicon glyphicon-align-justify"></a></div>
 </div>
-
